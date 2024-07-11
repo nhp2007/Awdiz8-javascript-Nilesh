9d9a8b409d4017eb576eb884b92981b4a5c4c8a3
nhp2007 <165290650+nhp2007@users.noreply.github.com>
2024-07-11 09:07:52 +0530

    update

---
+++ Day-1/index.js
@@ -0,0 +1,70 @@
+console.log(typeof("awdiz"))
+console.log(typeof(`awdiz`))
+console.log(typeof('awdiz'))
+
+console.log(typeof(2342))
+console.assert(typeof("2475763"))
+
+console.log(typeof(true))
+console.log(typeof(false))
+
+var username = "awdiz"
+var userage = 18
+var usermail = "awdiz@gmail.com"
+var userpass = "awd1234"
+var user
+
+console.log(username, userage, usermail, userpass)
+console.log(user, typeof(user))
+
+
+var num1 = 213
+var num2 = 978
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+
+var num = 20
+console.log(num)
+num = 10
+console.log(num)
+var num = 17
+console.log(num)
+
+
+// let num = 20
+// console.log(num)
+// num = 10
+// console.log(num)
+// let num = 17
+// console.log(num)
+
+
+// const num = 20
+// console.log(num)
+// num = 10
+// console.log(num)
+// const num = 17
+// console.log(num)
+
+
+// {
+//     var num6 =207
+//     let num7 =321
+//     console.log(num7)
+//     const num9= 17
+// }
+// console.log(num6)
+// console.log(num7)
+// console.log(num9)
+
+
+{
+    const num8 = 10
+    {console.log(num8)}
+}
+// console.log(num8)
---
+++ Day-2/index.js
@@ -0,0 +1,45 @@
+number = 10;
+console.log(number);
+var number;
+
+console.log(10 < 20);
+console.log(10 > 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+console.log(10 <= 10);
+
+console.log(2 == "2");
+console.log(2 === "2");
+{
+if (false) {
+  console.log("if block");
+} else {
+  "else block";
+}
+}
+if (true) {
+  console.log("if block");
+} else {
+  console.log("else block");
+}
+
+if (10 > 20) {
+  console.log("inside if");
+} else {
+  console.log("inside else");
+}
+
+var num1 = 345;
+var num2 = 435;
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("num1 is lesser than num2");
+}
+
+if (num1 < num2) {
+    console.log("num1 is greater than num2");
+  } else {
+    console.log("num1 is lesser than num2");
+  }
