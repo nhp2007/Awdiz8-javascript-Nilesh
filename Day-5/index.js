04eacb4f3e11ad60afa720201f19d555e9c67951
nhp2007 <165290650+nhp2007@users.noreply.github.com>
2024-07-16 09:20:03 +0530

    Updates

---
+++ Day-5/index.js
@@ -0,0 +1,40 @@
+const isNumberEvenorOdd = (number) => {
+    if (number % 2 == 0){
+        console.log(`number ${number} is even`)
+    }else{
+        console.log(` number ${number} is odd`)
+    }
+}
+
+isNumberEvenorOdd(15)
+
+
+for (var i = 1;i<=10;i++){
+    console.log(i)
+}
+
+
+console.log("---------ASSIGNMENT------------")
+
+//Q1//
+console.log("----------------------Q1------------------------")
+
+
+for (var i = 20; i <= 40;i++){
+    console.log(i)
+}
+
+//Q2//
+console.log("----------------------Q2------------------------")
+
+
+for (var j = 1;j<=100;j+=2)
+    {
+    console.log(j)
+}
+
+//Q3//
+console.log("----------------------Q3------------------------")
+for (var k=50;k<=150;k+=10){
+    console.log(k)
+}
